574d5601bf1d6883cd111dc2fcbf35c177304fe3
paranoiaoops <imathinker.oops@gmail.com>
2020-11-11 14:24:35 +0900

    テストパターンの追加&フォーマットの実行

--- src/Main.elm
+++ src/Main.elm
@@ -7,7 +7,10 @@ import Html.Events exposing (..)
 import Random
 import Random.List exposing (shuffle)
 
+
+
 -- MAIN
+
 
 main =
     Browser.element
@@ -15,15 +18,18 @@ main =
         , view = view
         , update = update
         , subscriptions = subscriptions
-    }
+        }
+
 
 
 -- MODEL
+
 
 type GameMode
     = Menu
     | Playing
     | End
+
 
 type CardType
     = Heart
@@ -31,25 +37,31 @@ type CardType
     | Spade
     | Diamond
 
+
 type alias PlayingCard =
     { cardType : CardType
-    , number : Int}
+    , number : Int
+    }
+
 
 type alias Model =
     { deck : List PlayingCard
     , dealer : List PlayingCard
     , player : List PlayingCard
-    , status : GameMode}
+    , status : GameMode
+    }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
     ( Model [] [] [] Menu
     , Cmd.none
     )
 
 
+
 -- UPDATE
+
 
 type Msg
     = Hit
@@ -58,20 +70,21 @@ type Msg
     | CardSetUp (List PlayingCard)
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GameStart ->
             ( model
-            , Random.generate CardSetUp (shufflePlayingCardsList createPlayingCardsList))
+            , Random.generate CardSetUp (shufflePlayingCardsList createPlayingCardsList)
+            )
 
         CardSetUp playingCards ->
             ( { model
                 | dealer = List.take 2 playingCards
                 , player = List.take 2 (List.drop 2 playingCards)
-                , deck = (List.drop 4 playingCards )
+                , deck = List.drop 4 playingCards
                 , status = Playing
-               }
+              }
             , Cmd.none
             )
 
@@ -79,126 +92,170 @@ update msg model =
             ( { model
                 | player = model.player ++ List.take 1 model.deck
                 , deck = List.drop 1 model.deck
-                }
+              }
             , Cmd.none
             )
 
         Stand ->
             let
-                newModel = dealerDrawCards model
+                newModel =
+                    dealerDrawCards model
             in
-                ( { newModel
-                    | status = End
-                }
-                , Cmd.none
-                )
+            ( { newModel
+                | status = End
+              }
+            , Cmd.none
+            )
+
+
 
 -- SUBSCRIPTION
+
+
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
 
 
+
 -- VIEW
+
 
 view : Model -> Html Msg
 view model =
     div []
-        [ header [] [text "elm で ブラックジャック"]
+        [ header [] [ text "elm で ブラックジャック" ]
         , case model.status of
             Menu ->
                 div []
-                    [ button [onClick GameStart] [text "Game Start"]
+                    [ button [ onClick GameStart ] [ text "Game Start" ]
                     ]
+
             Playing ->
                 div []
                     [ div []
-                        [ div [] [text "ディーラー"]
+                        [ div [] [ text "ディーラー" ]
                         , ul []
-                            (List.map (\l -> li [] [ text (String.fromInt l.number)]) model.dealer)
+                            (List.map (\l -> li [] [ text (String.fromInt l.number) ]) model.dealer)
                         ]
                     , div []
-                        [ div [] [text "プレイヤー"]
+                        [ div [] [ text "プレイヤー" ]
                         , ul []
                             (List.append
-                              (List.map (\l -> li [] [ text (String.fromInt l.number)]) model.player)
-                              [li [] [text ("Total : " ++ (String.fromInt (calculatePoint model.player)))]]
+                                (List.map (\l -> li [] [ text (String.fromInt l.number) ]) model.player)
+                                [ li [] [ text ("Total : " ++ String.fromInt (calculatePoint model.player)) ] ]
                             )
                         , div []
-                            [button [onClick Hit] [text "Hit"]
-                            , button [onClick Stand] [text "Stand"]
+                            [ button [ onClick Hit ] [ text "Hit" ]
+                            , button [ onClick Stand ] [ text "Stand" ]
                             ]
                         ]
                     ]
+
             End ->
                 div []
                     [ text ("プレイヤーの" ++ judgment model.player model.dealer)
                     , div []
-                        [ text ("ディラー：" ++ (String.fromInt (calculatePoint model.dealer)))]
+                        [ text ("ディラー：" ++ String.fromInt (calculatePoint model.dealer)) ]
                     , div []
-                        [ text ("プレイヤー：" ++ (String.fromInt (calculatePoint model.player)))]
+                        [ text ("プレイヤー：" ++ String.fromInt (calculatePoint model.player)) ]
                     , div []
-                        [button [onClick GameStart] [text "もう一回"]]
+                        [ button [ onClick GameStart ] [ text "もう一回" ] ]
                     ]
-
         ]
+
 
 
 -- Module
 -- 山札の生成
+
+
 createPlayingCardsList : List PlayingCard
 createPlayingCardsList =
-   List.map (\n -> PlayingCard Heart n) (List.range 1 13)
-    ++ List.map (\n -> PlayingCard Clover n) (List.range 1 13)
-    ++ List.map (\n -> PlayingCard Diamond n) (List.range 1 13)
-    ++ List.map (\n -> PlayingCard Spade n) (List.range 1 13)
+    List.map (\n -> PlayingCard Heart n) (List.range 1 13)
+        ++ List.map (\n -> PlayingCard Clover n) (List.range 1 13)
+        ++ List.map (\n -> PlayingCard Diamond n) (List.range 1 13)
+        ++ List.map (\n -> PlayingCard Spade n) (List.range 1 13)
 
-shufflePlayingCardsList : List PlayingCard ->  Random.Generator (List PlayingCard)
-shufflePlayingCardsList cards = shuffle cards
+
+shufflePlayingCardsList : List PlayingCard -> Random.Generator (List PlayingCard)
+shufflePlayingCardsList cards =
+    shuffle cards
+
 
 calculatePoint : List PlayingCard -> Int
 calculatePoint cards =
     let
         cardNumbers =
             List.map getCardNumber cards
-            |> List.map convertOverEleven
+                |> List.map convertOverEleven
+
         noAceTotal =
             List.filter (\n -> n /= 1) cardNumbers
-            |> List.foldl (+) 0
+                |> List.foldl (+) 0
 
-        isAce = List.member 1 cardNumbers
+        aceCount =
+            List.filter (\n -> n == 1) cardNumbers
+                |> List.foldl (+) 0
+
+        isAce =
+            List.member 1 cardNumbers
     in
-       if isAce then
-         if noAceTotal > 10 then noAceTotal + 1
-         else noAceTotal + 11
-       else
-         List.foldl (+) 0 cardNumbers
+    if isAce then
+        if noAceTotal > 10 then
+            noAceTotal + aceCount
+
+        else
+            noAceTotal + 10 + aceCount
+
+    else
+        List.foldl (+) 0 cardNumbers
 
 
 convertOverEleven : Int -> Int
-convertOverEleven number = if number >= 11 then 10 else number
+convertOverEleven number =
+    if number >= 11 then
+        10
+
+    else
+        number
+
 
 getCardNumber : PlayingCard -> Int
-getCardNumber card = card.number
+getCardNumber card =
+    card.number
+
 
 dealerDrawCards : Model -> Model
 dealerDrawCards model =
-    if calculatePoint model.dealer >= 17 then model
+    if calculatePoint model.dealer >= 17 then
+        model
+
     else
-        dealerDrawCards { model
-        | dealer = model.dealer ++ List.take 1 model.deck
-        , deck = List.drop 1 model.deck
-        }
+        dealerDrawCards
+            { model
+                | dealer = model.dealer ++ List.take 1 model.deck
+                , deck = List.drop 1 model.deck
+            }
+
 
 judgment : List PlayingCard -> List PlayingCard -> String
 judgment playerCards dealerCards =
     let
         playerPoint =
             calculatePoint playerCards
+
         dealerPoint =
             calculatePoint dealerCards
     in
-        if playerPoint > 21 then "プレイヤーの負け"
-        else if dealerPoint > 21 then "プレイヤーの負け"
-        else if dealerPoint < playerPoint then "プレイヤーの勝ち"
-        else "プレイヤーの負け"
+    if playerPoint > 21 then
+        "プレイヤーの負け"
+
+    else if dealerPoint > 21 then
+        "プレイヤーの負け"
+
+    else if dealerPoint < playerPoint then
+        "プレイヤーの勝ち"
+
+    else
+        "プレイヤーの負け"
--- tests/Example.elm
+++ tests/Example.elm
@@ -1,10 +1,344 @@
 module Example exposing (..)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
+import Main
 import Test exposing (..)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    describe "Card control module"
+        [ describe "convert over eleven card"
+            [ test "convert" <|
+                \_ ->
+                    Main.convertOverEleven 11
+                        |> Expect.equal 10
+            , test "not concert" <|
+                \_ ->
+                    Main.convertOverEleven 10
+                        |> Expect.equal 10
+            ]
+        , describe "calculate black jack point"
+            [ test "no ace and no eleven over no 1" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 3
+                            , Main.PlayingCard Main.Heart 5
+                            , Main.PlayingCard Main.Heart 10
+                            , Main.PlayingCard Main.Heart 2
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 20
+            , test "no ace and no eleven over no 2" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 3
+                            , Main.PlayingCard Main.Heart 5
+                            , Main.PlayingCard Main.Heart 10
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 18
+            , test "ace equal eleven no 1" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 1
+                            , Main.PlayingCard Main.Heart 5
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 16
+            , test "ace equal eleven no 2" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 1
+                            , Main.PlayingCard Main.Heart 5
+                            , Main.PlayingCard Main.Heart 5
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 21
+            , test "ace equal eleven no 3" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 1
+                            , Main.PlayingCard Main.Heart 2
+                            , Main.PlayingCard Main.Heart 4
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 17
+            , test "ace equal eleven no 4" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 1
+                            , Main.PlayingCard Main.Heart 2
+                            , Main.PlayingCard Main.Heart 4
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 17
+            , test "ace two card" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 1
+                            , Main.PlayingCard Main.Heart 2
+                            , Main.PlayingCard Main.Clover 1
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 14
+            , test "Black Jack" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 1
+                            , Main.PlayingCard Main.Clover 13
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 21
+            , test "no ace, eleven over no 1" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 13
+                            , Main.PlayingCard Main.Clover 5
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 15
+            , test "no ace, eleven over no 2" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 13
+                            , Main.PlayingCard Main.Clover 2
+                            , Main.PlayingCard Main.Clover 5
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 17
+            , test "ace, eleven over no 1" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 11
+                            , Main.PlayingCard Main.Clover 2
+                            , Main.PlayingCard Main.Clover 1
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 13
+            , test "ace, eleven over no 2" <|
+                \_ ->
+                    let
+                        testCardList =
+                            [ Main.PlayingCard Main.Heart 12
+                            , Main.PlayingCard Main.Clover 5
+                            , Main.PlayingCard Main.Clover 8
+                            , Main.PlayingCard Main.Clover 1
+                            ]
+                    in
+                    Main.calculatePoint testCardList
+                        |> Expect.equal 24
+            ]
+        , describe "dealer logic"
+            [ test "not draw pattern no 1" <|
+                \_ ->
+                    let
+                        dummyModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 5
+                                , Main.PlayingCard Main.Heart 10
+                                , Main.PlayingCard Main.Heart 3
+                                ]
+                                [ Main.PlayingCard Main.Diamond 7
+                                , Main.PlayingCard Main.Diamond 10
+                                ]
+                                []
+                                Main.End
+
+                        expectModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 5
+                                , Main.PlayingCard Main.Heart 10
+                                , Main.PlayingCard Main.Heart 3
+                                ]
+                                [ Main.PlayingCard Main.Diamond 7
+                                , Main.PlayingCard Main.Diamond 10
+                                ]
+                                []
+                                Main.End
+                    in
+                    Main.dealerDrawCards dummyModel
+                        |> Expect.equal expectModel
+            , test "not draw pattern no 2" <|
+                \_ ->
+                    let
+                        dummyModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 5
+                                , Main.PlayingCard Main.Heart 10
+                                , Main.PlayingCard Main.Heart 3
+                                ]
+                                [ Main.PlayingCard Main.Diamond 4
+                                , Main.PlayingCard Main.Diamond 10
+                                , Main.PlayingCard Main.Diamond 3
+                                ]
+                                []
+                                Main.End
+
+                        expectModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 5
+                                , Main.PlayingCard Main.Heart 10
+                                , Main.PlayingCard Main.Heart 3
+                                ]
+                                [ Main.PlayingCard Main.Diamond 4
+                                , Main.PlayingCard Main.Diamond 10
+                                , Main.PlayingCard Main.Diamond 3
+                                ]
+                                []
+                                Main.End
+                    in
+                    Main.dealerDrawCards dummyModel
+                        |> Expect.equal expectModel
+            , test "draw pattern no 1" <|
+                \_ ->
+                    let
+                        dummyModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 13
+                                , Main.PlayingCard Main.Heart 11
+                                , Main.PlayingCard Main.Heart 3
+                                ]
+                                [ Main.PlayingCard Main.Diamond 4
+                                , Main.PlayingCard Main.Diamond 3
+                                ]
+                                []
+                                Main.End
+
+                        expectModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 11
+                                , Main.PlayingCard Main.Heart 3
+                                ]
+                                [ Main.PlayingCard Main.Diamond 4
+                                , Main.PlayingCard Main.Diamond 3
+                                , Main.PlayingCard Main.Heart 13
+                                ]
+                                []
+                                Main.End
+                    in
+                    Main.dealerDrawCards dummyModel
+                        |> Expect.equal expectModel
+            , test "draw pattern no 2" <|
+                \_ ->
+                    let
+                        dummyModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 3
+                                , Main.PlayingCard Main.Heart 5
+                                , Main.PlayingCard Main.Heart 6
+                                , Main.PlayingCard Main.Heart 4
+                                , Main.PlayingCard Main.Heart 8
+                                ]
+                                [ Main.PlayingCard Main.Diamond 4
+                                , Main.PlayingCard Main.Diamond 3
+                                ]
+                                []
+                                Main.End
+
+                        expectModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 4
+                                , Main.PlayingCard Main.Heart 8
+                                ]
+                                [ Main.PlayingCard Main.Diamond 4
+                                , Main.PlayingCard Main.Diamond 3
+                                , Main.PlayingCard Main.Heart 3
+                                , Main.PlayingCard Main.Heart 5
+                                , Main.PlayingCard Main.Heart 6
+                                ]
+                                []
+                                Main.End
+                    in
+                    Main.dealerDrawCards dummyModel
+                        |> Expect.equal expectModel
+            , test "draw pattern no 3" <|
+                \_ ->
+                    let
+                        dummyModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 1
+                                , Main.PlayingCard Main.Heart 5
+                                , Main.PlayingCard Main.Heart 6
+                                , Main.PlayingCard Main.Heart 4
+                                , Main.PlayingCard Main.Heart 8
+                                ]
+                                [ Main.PlayingCard Main.Diamond 4
+                                , Main.PlayingCard Main.Diamond 3
+                                ]
+                                []
+                                Main.End
+
+                        expectModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 5
+                                , Main.PlayingCard Main.Heart 6
+                                , Main.PlayingCard Main.Heart 4
+                                , Main.PlayingCard Main.Heart 8
+                                ]
+                                [ Main.PlayingCard Main.Diamond 4
+                                , Main.PlayingCard Main.Diamond 3
+                                , Main.PlayingCard Main.Heart 1
+                                ]
+                                []
+                                Main.End
+                    in
+                    Main.dealerDrawCards dummyModel
+                        |> Expect.equal expectModel
+            , test "draw pattern no 4" <|
+                \_ ->
+                    let
+                        dummyModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 1
+                                , Main.PlayingCard Main.Heart 2
+                                , Main.PlayingCard Main.Heart 6
+                                , Main.PlayingCard Main.Heart 4
+                                , Main.PlayingCard Main.Heart 8
+                                ]
+                                [ Main.PlayingCard Main.Diamond 3
+                                , Main.PlayingCard Main.Diamond 2
+                                ]
+                                []
+                                Main.End
+
+                        expectModel =
+                            Main.Model
+                                [ Main.PlayingCard Main.Heart 6
+                                , Main.PlayingCard Main.Heart 4
+                                , Main.PlayingCard Main.Heart 8
+                                ]
+                                [ Main.PlayingCard Main.Diamond 3
+                                , Main.PlayingCard Main.Diamond 2
+                                , Main.PlayingCard Main.Heart 1
+                                , Main.PlayingCard Main.Heart 2
+                                ]
+                                []
+                                Main.End
+                    in
+                    Main.dealerDrawCards dummyModel
+                        |> Expect.equal expectModel
+            ]
+        ]
